c56c6705a5938e59f46460655becf882f053516d
Sandro Mancuso <sandromancuso@yahoo.com>
2016-03-07 23:37:50 +0000

    Interesting. Changed acceptance test to minimise mocking. Instead of mocking Dice, I only mocked RandomDieValue

--- src/test/scala/acceptance/SinglePlayerPlaysOneCategory.scala
+++ src/test/scala/acceptance/SinglePlayerPlaysOneCategory.scala
@@ -9,14 +9,15 @@ class SinglePlayerPlaysOneCategory extends UnitSpec {
 
 	"single player plays one category" in {
 		val console = mock[Console]
-		val dice = mock[Dice]
+		val randomDieValue = mock[RandomDieValue]
+		val dice = new Dice(randomDieValue)
 		val one_category = Category("Ones")
 		val turn = new Turn(dice, console)
 
-		given(dice roll()) willReturn(
-				"D1:2 D2:4 D3:1 D4:6 D5:1",
-				"D1:1 D2:5 D3:1 D4:2 D5:1",
-				"D1:1 D2:1 D3:1 D4:5 D5:1")
+		given(randomDieValue next()) willReturn(
+				2, 4, 1, 6, 1,
+				1, 5, 2,
+				1, 5)
 
 		given(console read("Dices to re-run: ")) willReturn(
 				"D1 D2 D4",
