4e94d5770abc47a28a8920965901d6cb924dec09
Sandro Mancuso <sandromancuso@yahoo.com>
2016-03-07 18:01:57 +0000

    Acceptance test for single player, single category

--- src/main/scala/com/codurance/yahtzee/Console.scala
+++ src/main/scala/com/codurance/yahtzee/Console.scala
@@ -1,7 +1,14 @@
 package com.codurance.yahtzee
 
+import scala.io.StdIn
+
 class Console {
 
 	def printLine(text: String): Unit = println(text)
+
+	def read(text: String): String = {
+		print(text)
+		StdIn.readLine()
+	}
 
 }
---
+++ src/test/scala/acceptance/SinglePlayerPlaysOneCategory.scala
@@ -0,0 +1,39 @@
+package acceptance
+
+import com.codurance.UnitSpec
+import com.codurance.yahtzee._
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.verify
+
+class SinglePlayerPlaysOneCategory extends UnitSpec {
+
+	"single player plays one category" in {
+		val console = mock[Console]
+		val one_category = Category("Ones")
+		val score = new Score
+		val turn = new Turn
+
+		given(console read("Dices to re-run: ")) willReturn(
+				"D1 D2 D4",
+				"D2 D4")
+
+		val yahtzee = new Yahtzee(one_category, score, turn, console)
+
+		yahtzee startGame()
+
+		verify(console) printLine("Category: Ones")
+		verify(console) printLine("Dices: D1:2 D2:4 D3:1 D4:6 D5:1")
+		verify(console) printLine("Dices to re-run: D1 D2 D4")
+		verify(console) printLine("Dices: D1:1 D2:5 D3:1 D4:2 D5:1")
+		verify(console) printLine("Dices to re-run: D2 D4")
+		verify(console) printLine("Dices: D1:1 D2:1 D3:1 D4:5 D5:1")
+		verify(console) printLine("Category Ones score: 4")
+
+		verify(console) printLine("Yahtzee score")
+		verify(console) printLine("Ones: 4")
+		verify(console) printLine("Final score: 4")
+
+	}
+
+
+}
