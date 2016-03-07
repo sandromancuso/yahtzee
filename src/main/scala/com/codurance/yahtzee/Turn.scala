e3319720a89b9c236614ec02352c2380944786ed
Sandro Mancuso <sandromancuso@yahoo.com>
2016-03-07 19:37:47 +0000

    Turn now asks for two dice re-run. Old tests deleted because became redundant. Dice roll result still not being used

--- src/main/scala/com/codurance/yahtzee/Turn.scala
+++ src/main/scala/com/codurance/yahtzee/Turn.scala
@@ -3,10 +3,15 @@ package com.codurance.yahtzee
 class Turn(dice: Dice, console: Console) {
 
 	def start(category: Category): Score = {
-		val rollResult = dice roll()
+		var rollResult = dice roll()
 
 		console printLine(s"Dice: ${rollResult}")
 		console read("Dice to re-run: ")
+		rollResult = dice roll()
+		console printLine(s"Dice: ${rollResult}")
+		console read("Dice to re-run: ")
+		rollResult = dice roll()
+		console printLine(s"Dice: ${rollResult}")
 
 		Score(category, 2)
 	}
--- src/test/scala/com/codurance/yahtzee/TurnShould.scala
+++ src/test/scala/com/codurance/yahtzee/TurnShould.scala
@@ -17,22 +17,20 @@ class TurnShould extends UnitSpec {
 		val turn = new Turn(dice, console)
 	}
 
-	"Prints the first dice roll after it starts" in new context {
-		given(dice roll()) willReturn("D1:2 D2:4 D3:1 D4:6 D5:1")
-
-		turn start(category)
-
-		verify(console) printLine("Dice: D1:2 D2:4 D3:1 D4:6 D5:1")
-	}
-
-	"Asks for dice re-runs after displaying the first dice rool" in new context {
-		given(dice roll()) willReturn("D1:2 D2:4 D3:1 D4:6 D5:1")
+	"Runs a full turn with two dice re-run" in new context {
+		given(dice roll()) willReturn(
+				"D1:2 D2:4 D3:1 D4:6 D5:1",
+				"D1:1 D2:5 D3:1 D4:2 D5:1",
+				"D1:1 D2:1 D3:1 D4:5 D5:1")
 
 		turn start(category)
 
 		val inOrder = Mockito.inOrder(console)
 		inOrder.verify(console) printLine("Dice: D1:2 D2:4 D3:1 D4:6 D5:1")
 		inOrder.verify(console) read("Dice to re-run: ")
+		inOrder.verify(console) printLine("Dice: D1:1 D2:5 D3:1 D4:2 D5:1")
+		inOrder.verify(console) read("Dice to re-run: ")
+		inOrder.verify(console) printLine("Dice: D1:1 D2:1 D3:1 D4:5 D5:1")
 	}
 
 	"return the score of that turn" in new context {
