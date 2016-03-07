f26884536b19e37deabf4f54fe7f217942cebd49
Sandro Mancuso <sandromancuso@yahoo.com>
2016-03-07 19:23:17 +0000

    Fixed SinglePlayerPlaysOneCategory test to add console reads

--- src/test/scala/acceptance/SinglePlayerPlaysOneCategory.scala
+++ src/test/scala/acceptance/SinglePlayerPlaysOneCategory.scala
@@ -28,9 +28,9 @@ class SinglePlayerPlaysOneCategory extends UnitSpec {
 
 		verify(console) printLine("Category: Ones")
 		verify(console) printLine("Dice: D1:2 D2:4 D3:1 D4:6 D5:1")
-		verify(console) printLine("Dice to re-run: ")
+		verify(console) read("Dice to re-run: ")
 		verify(console) printLine("Dice: D1:1 D2:5 D3:1 D4:2 D5:1")
-		verify(console) printLine("Dice to re-run: ")
+		verify(console) read("Dice to re-run: ")
 		verify(console) printLine("Dice: D1:1 D2:1 D3:1 D4:5 D5:1")
 		verify(console) printLine("Category Ones score: 4")
 
