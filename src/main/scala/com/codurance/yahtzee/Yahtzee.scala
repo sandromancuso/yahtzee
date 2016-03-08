3525c3487b60f388eaf403e00c4ce708633b3be3
Sandro Mancuso <sandromancuso@yahoo.com>
2016-03-08 03:05:48 +0000

    Code works for multiple categories. Minor refactoring on Yahtzee class

--- src/main/scala/com/codurance/yahtzee/Yahtzee.scala
+++ src/main/scala/com/codurance/yahtzee/Yahtzee.scala
@@ -5,9 +5,9 @@ class Yahtzee(scoreCard: ScoreCard,
               console: Console) {
 
 	def startGame(): Unit = {
-		scoreCard.categories.foreach(c => {
-			console.printLine(s"Category: ${c.name}")
-			scoreCard.setScore(turn start c)
+		scoreCard.categories.foreach(category => {
+			console.printLine(s"Category: ${category.name}")
+			scoreCard.setScore(turn start category)
 		})
 		scoreCard print console
 	}
