fc26deb8b0be95080d5a322ddefe7d6401d2a399
Sandro Mancuso <sandromancuso@yahoo.com>
2016-03-08 01:16:00 +0000

    Acceptance test passed

--- src/main/scala/com/codurance/yahtzee/Turn.scala
+++ src/main/scala/com/codurance/yahtzee/Turn.scala
@@ -4,13 +4,13 @@ class Turn(dice: Dice, console: Console) {
 
 	def start(category: Category): Score = {
 		var rollResult = dice rollAll()
-		console printLine(s"Dice: ${rollResult}")
+		console printLine s"Dice: ${rollResult}"
 
-		rollResult = dice roll (console read ("[1] Dice to re-run: "))
-		console printLine(s"Dice: ${rollResult}")
+		rollResult = dice roll (console read "[1] Dice to re-run: ")
+		console printLine s"Dice: ${rollResult}"
 
-		rollResult = dice roll(console read("[2] Dice to re-run: "))
-		console printLine(s"Dice: ${rollResult}")
+		rollResult = dice roll(console read "[2] Dice to re-run: ")
+		console printLine s"Dice: ${rollResult}"
 
 		Score(category, category.pointsFor(rollResult))
 	}
--- src/test/scala/acceptance/SinglePlayerPlaysOneCategory.scala
+++ src/test/scala/acceptance/SinglePlayerPlaysOneCategory.scala
@@ -19,25 +19,24 @@ class SinglePlayerPlaysOneCategory extends UnitSpec {
 				1, 5, 2,
 				1, 5)
 
-		given(console read("Dices to re-run: ")) willReturn(
-				"D1 D2 D4",
-				"D2 D4")
+		given(console read "[1] Dice to re-run: ") willReturn "D1 D2 D4"
+		given(console read "[2] Dice to re-run: ") willReturn "D2 D4"
 
 		val yahtzee = new Yahtzee(one_category, turn, console)
 
 		yahtzee startGame()
 
 		val inOrder = Mockito.inOrder(console)
-		inOrder.verify(console) printLine("Category: Ones")
-		inOrder.verify(console) printLine("Dice: D1:2 D2:4 D3:1 D4:6 D5:1")
-		inOrder.verify(console) read("[1] Dice to re-run: ")
-		inOrder.verify(console) printLine("Dice: D1:1 D2:5 D3:1 D4:2 D5:1")
-		inOrder.verify(console) read("[2] Dice to re-run: ")
-		inOrder.verify(console) printLine("Dice: D1:1 D2:1 D3:1 D4:5 D5:1")
+		inOrder.verify(console) printLine "Category: Ones"
+		inOrder.verify(console) printLine "Dice: D1:2 D2:4 D3:1 D4:6 D5:1"
+		inOrder.verify(console) read "[1] Dice to re-run: "
+		inOrder.verify(console) printLine "Dice: D1:1 D2:5 D3:1 D4:2 D5:1"
+		inOrder.verify(console) read "[2] Dice to re-run: "
+		inOrder.verify(console) printLine "Dice: D1:1 D2:1 D3:1 D4:5 D5:1"
 
-		inOrder.verify(console) printLine("Yahtzee score")
-		inOrder.verify(console) printLine("Ones: 4")
-		inOrder.verify(console) printLine("Final score: 4")
+		inOrder.verify(console) printLine "Yahtzee score"
+		inOrder.verify(console) printLine "Ones: 4"
+		inOrder.verify(console) printLine "Final score: 4"
 	}
 
 
